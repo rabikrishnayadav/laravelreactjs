9ceb4d737b41e606827ae9183821b2995f0451c9
Rabi Krishna Yadav <rabikumar23154@gmail.com>
2024-02-14 01:38:41 -0800

    public files change and reactjs install

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        outDir:'build'
+    },
     plugins: [
+        react(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
